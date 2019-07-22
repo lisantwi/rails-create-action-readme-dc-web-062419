e04e6f36dd79a734af7f6e431ffccce2b42a74c5
Lisa Antwi Adjei <lisaantwi@gmail.com>
2019-07-22 03:55:56 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :new, :create]
-  get 'posts/:id', to: 'posts#show'
+  resources :posts, only: [:index, :show :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
